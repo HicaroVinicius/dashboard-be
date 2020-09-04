8f9609c40f8d15f92f647ac8be51ac80c52f5fa2
HicaroVinicius <hicaro_vinicius@hotmail.com>
2020-09-03 22:20:31 -0300

    socket

--- app.js
+++ app.js
@@ -1,35 +1,157 @@
+var debug = require('debug')('angular2-nodejs:server');
+var http = require('http');
 const express = require('express');
-const socket = require('socket.io');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: '*' }));
 app.use(bodyParser);
-let x = true;
 
-const server = app.listen(3000,() => {
-    console.log('Started in 3000');
+// const server = app.listen(3000, () => {
+//     console.log('Started in 3000');
+// });
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
+
+io.on('connection',(socket)=>{
+
+    console.log('new connection made.');
+
+
+    socket.on('join', function(data){
+      //joining
+      socket.join(data.room);
+
+      console.log(data.user + 'joined the room : ' + data.room);
+
+      socket.broadcast.to(data.room).emit('new user joined', {user:data.user, message:'has joined this room.'});
+    });
+
+
+    socket.on('leave', function(data){
+    
+      console.log(data.user + 'left the room : ' + data.room);
+
+      socket.broadcast.to(data.room).emit('left room', {user:data.user, message:'has left this room.'});
+
+      socket.leave(data.room);
+    });
+
+    socket.on('message',function(data){
+
+      io.in(data.room).emit('new message', {user:data.user, message:data.message});
+    })
 });
 
-const io = socket(server);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-io.sockets.on('connection', (socket) => {
-    console.log(`new connection id: ${socket.id}`);
-    sendData(socket);
-})
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-function sendData(socket){
-    
-    if(x){
-        socket.emit('data1', Array.from({length: 8}, () => Math.floor(Math.random() * 590)+ 10));
-        x = !x;
-    }else{
-        socket.emit('data2', Array.from({length: 8}, () => Math.floor(Math.random() * 590)+ 10));
-        x = !x;
-    }
-    console.log(`data is ${x}`);
-    setTimeout(() => {
-        sendData(socket);
-    }, 10000);
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
 }
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
+// const express = require('express');
+// const socket = require('socket.io');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+
+// const app = express();
+// app.use(cors({ origin: '*' }));
+// app.use(bodyParser);
+// let x = true;
+
+// const server = app.listen(3000, () => {
+//     console.log('Started in 3000');
+// });
+
+// const io = socket(server);
+
+// io.sockets.on('connection', (socket) => {
+//     console.log(`new connection id: ${socket.id}`);
+//     socket.on('disconnect', function () {
+//         console.log(`user disconnect id: ${socket.id}`);
+//     });
+
+//     socket.on('message', function (message) {
+//         console.log(`Mensagem recebida: ${message}`);
+//         io.emit('message', { type: 'new-message', text: message });
+//     });
+
+// });
+
